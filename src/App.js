d5047d98f6ef0b08f442205a723a58eb8f50fc66
HiHighs <degroote.jonas@hotmail.com>
2024-03-05 20:11:02 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest my new income 🤑',
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function previous() {
+    if (1 < step) setStep((value) => value - 1);
+  }
+
+  function next() {
+    if (step < messages.length) setStep((value) => value + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className='close' onClick={() => setIsOpen((value) => !value)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className='steps'>
+          <div className='numbers'>
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
+
+          <p className='message'>
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className='buttons'>
+            <button
+              onClick={previous}
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={next}
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
